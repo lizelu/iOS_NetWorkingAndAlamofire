21c3c29812f1a77ba9bd6fe48200b2d5799903b5
zeluliCredit <zeluli@creditease.cn>
2016-05-19 11:42:49 +0800

    添加Alamofire相关控制器

---
+++ AlamofireDemo/AlamofireDemo/AlamofireViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AlamofireViewController.swift
+//  AlamofireDemo
+//
+//  Created by Mr.LuDashi on 16/5/19.
+//  Copyright © 2016年 ZeluLi. All rights reserved.
+//
+
+import UIKit
+
+class AlamofireViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
